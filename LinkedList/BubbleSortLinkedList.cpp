4e5d74d22ec881155826d37bc82c999a266b4df2
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-09-07 18:44:55 +0500

    Improved: Bubble sort on SIngle LL

--- LinkedList/BubbleSortLinkedList.cpp
+++ LinkedList/BubbleSortLinkedList.cpp
@@ -57,9 +57,10 @@ class LinkedList
         {
             int size =0;
             Node*temp = head;
-            while(temp != head)
+            while(temp != NULL)
             {
                 size++; 
+                temp = temp->next;
             }
 
             return size;
@@ -92,6 +93,7 @@ class LinkedList
             //it have n-1 iterations 
             for(int i=0;i<size-1;i++)
             {
+                bool swapped = false;
                 Node *curr = head;
                 //every iteration puts the large element in its correct position
                 for(int j=0;j<size-i-1;j++)
@@ -101,11 +103,17 @@ class LinkedList
                         int temp = curr->data;
                         curr->data = curr->next->data;
                         curr->next->data = temp;
+                        swapped = true;
                     }
 
                     curr = curr->next;
 
                 }
+                
+                if(!swapped)
+                {
+                	return;
+				}
             }
         }
 
@@ -131,6 +139,7 @@ int main()
     cout<<endl;
 
     list.bubblesort();
+    
     cout<<"\n===After Sorting===\n";
     list.display();
     cout<<endl;
